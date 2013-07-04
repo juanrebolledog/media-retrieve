057f47a8c1c9400907d70c5179bc14296fc0fe29
Juan Francisco Rebolledo Gómez <juanrebolledog@gmail.com>
2013-07-03 19:42:02 -0430

    Using strictly sqlite, updated config file example

--- main.py
+++ main.py
@@ -25,12 +25,12 @@ logger.setLevel(logging.INFO)
 
 
 conn = sqlite3.connect('media-retrieve.db')
-conn.text_factory = str
+conn.row_factory = sqlite3.Row
 c = conn.cursor()
 
 
 def parse_library(options):
-    #reset_db(options)
+    reset_db(options)
     logger.info('Reading Library')
     if options.verbose:
         print 'Reading Library'
@@ -44,32 +44,45 @@ def parse_library(options):
     for song in l.songs:
 
         h = HTMLParser.HTMLParser()
-        location = h.unescape(song.location)
+        location = h.unescape(song.location.decode('utf-8'))
         
         track = {
-            'track': song.name,
-            'artist': song.artist,
-            'album_artist': song.album_artist,
-            'album': song.album,
+            'track': song.name.decode('utf-8'),
             'kind': song.kind,
-            'location': urlparse(unquote(location), allow_fragments=False).path
+            'filename': urlparse(unquote(location), allow_fragments=False).path
         }
+
+        if song.album_artist:
+            track['album_artist'] = song.album_artist.decode('utf-8')
+        else:
+            track['album_artist'] = None
+
+        if song.artist:
+            track['artist'] = song.artist.decode('utf-8')
+        else:
+            track['artist'] = None
+
+        if song.album:
+            track['album'] = song.album.decode('utf-8')
+        else:
+            track['album'] = None
 
         options.artist = track['artist']
         options.album = track['album']
         options.track = track['track']
 
         if not find(options):
-            logger.info(track['location'])
+            logger.info(track['filename'])
             if options.verbose:
-                print track['location']
+                print track['filename']
 
             insert(track)
+    conn.commit()
     return True
 
 
 def insert(track):
-    track = (track['artist'], track['album'], track['album_artist'], track['track'], track['location'], track['kind'])
+    track = (track['artist'], track['album'], track['album_artist'], track['track'], track['filename'], track['kind'])
     c.execute('INSERT INTO tracks (artist, album, album_artist, track, filename, kind) VALUES (?, ?, ?, ?, ?, ?)', track)
 
 
@@ -108,30 +121,23 @@ def find(options):
         i += 1
         values = values + (conditions[field],)
 
-    result = c.execute(cmd, values)
+    c.execute(cmd, values)
 
-    print cmd
-    print values
-
-    if c.rowcount > 0:
-        return result
-
-    return False
+    return c.fetchall()
 
 
 def retrieve(options):
     songs = find(options)
-    song_left = song_count = songs.count()
-
-    def shellquote(s):
-        return s.replace(" ", "\\ ").replace("(", "\(").replace(")", "\)").replace("&", "\&")
+    song_left = song_count = len(songs)
 
     for song in songs:
         song_left -= 1
-        logger.info('[{1}/{2}] Copying "{0}"'.format(song['location'], song_left, song_count))
+        logger.info('[{1}/{2}] Copying "{0}"'.format(song['filename'], song_left, song_count))
+
         if options.verbose:
-            print '[{1}/{2}] Copying "{0}"'.format(song['location'], song_left, song_count)
-        file_name = os.path.basename(song['location'])
+            print '[{1}/{2}] Copying "{0}"'.format(song['filename'], song_left, song_count)
+
+        file_name = os.path.basename(song['filename'])
 
         if not os.path.isdir(CONFIG['app']['data_dir']):
             os.makedirs(CONFIG['app']['data_dir'])
@@ -143,20 +149,21 @@ def retrieve(options):
             os.makedirs(dir_name)
 
         if not os.path.exists(final_name) and not os.path.exists('{0}.mp3'.format(final_name)):
-            
+
             try:
                 if song['kind'] == 'Audio Apple Lossless':
-                    cmd = [ CONFIG['app']['xld_bin'], '-f', 'mp3', '-o', '{0}.mp3'.format(final_name), song['location'] ]
+                    cmd = [ CONFIG['app']['xld_bin'], '-f', 'mp3', '-o', '{0}.mp3'.format(final_name), song['filename'] ]
                     subprocess.call(cmd)
                 else:
-                    shutil.copyfile(song['location'], final_name)
+                    shutil.copyfile(song['filename'], final_name)
                 if options.verbose:
                     logger.info('Copy OK')
                     print 'Copy OK'
-            except Exception as e:
-                logger.warning('Copy failed for "{0}"'.format(song['location']))
+            except:
+                logger.warning('Copy failed for "{0}"'.format(song['filename']))
                 if options.verbose:
-                    print 'Copy failed for "{0}"'.format(song['location'])
+                    print 'Copy failed'
+
         else:
             logger.info('Already exists, skipping.')
             if options.verbose:
@@ -184,9 +191,12 @@ if __name__ == '__main__':
     if options.find:
         for result in find(options):
             print 'Track: {0} | Artist: {1} | Album: {2}'.format(result['track'], result['artist'], result['album'])
+
     elif options.scan:
         parse_library(options)
+
     elif options.reset:
         reset_db(options)
+
     elif options.retrieve:
         retrieve(options)
